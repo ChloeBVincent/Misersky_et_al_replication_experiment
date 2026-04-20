60a1b4d49c666c089ab49dabb6d9109f9a9eed5e
Chloé Vincent <chloe.vincent@ugent.be>
2026-04-20 12:28:27 +0200

    Create new file to deal with translation strings

--- norm.js
+++ norm.js
@@ -1,22 +1,3 @@
-var experiment_strings = all_translations[language];
-
-var options_age = [
-    '18–24',
-    '25–34',
-    '35–44',
-    '45–64',
-    '65+',
-    experiment_strings.option_pnts
-];
-
-var options_genre = [
-    experiment_strings.option_gender_male,
-    experiment_strings.option_gender_female,
-    experiment_strings.option_gender_nb,
-    experiment_strings.option_gender_other,
-    experiment_strings.option_pnts
-]
-
 function saveData(name, data){
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'record_result.php'); //
